fa6cd7b65683efcd03a4e576958f8ce8f649d58f
Maxim Volgin <maxim.volgin@me.com>
2017-06-15 09:09:37 +0200

    added macOS target

---
+++ macOS_RaspSwift/macOS_RaspSwift.h
@@ -0,0 +1,19 @@
+//
+//  macOS_RaspSwift.h
+//  macOS_RaspSwift
+//
+//  Created by Maxim Volgin on 15/06/2017.
+//  Copyright © 2017 Maxim Volgin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for macOS_RaspSwift.
+FOUNDATION_EXPORT double macOS_RaspSwiftVersionNumber;
+
+//! Project version string for macOS_RaspSwift.
+FOUNDATION_EXPORT const unsigned char macOS_RaspSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <macOS_RaspSwift/PublicHeader.h>
+
+
