1cfb5ad0722cc4a3ea12d22048f8acd54cc818e8
Maxim Volgin <maxvol@users.noreply.github.com>
2019-06-24 03:34:09 +0200

    Create Package.swift
    SPM support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "RaspSwift",
+    products: [
+        .library(
+            name: "RaspSwift",
+            targets: ["RaspSwift"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.1")
+    ],
+    targets: [
+        .target(
+            name: "RaspSwift",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "RaspSwiftTests",
+            dependencies: [
+                "RxSwift",
+                "RaspSwift"
+            ])
+    ]
+)
